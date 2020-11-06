e7ed436a06921d5633ade8bb481e79bfc8dbd7b4
mapeles <72173110+mapeles@users.noreply.github.com>
2020-11-06 15:15:07 +0900

    Delete AndroidProducts.mk

--- prebuilt/AndroidProducts.mk
+++
@@ -1,7 +0,0 @@
-# Replace $$DEVICE$$ with your Device Name's Value.
-# Replace $$BRAND$$ with your Brand's / Manufacturer's Value.
-# The part of last line in mine looks like "omni_Primo_RX5.mk"
-
-LOCAL_PATH := device/SAMSUNG/a8sqlteks
-
-PRODUCT_MAKEFILES := $(LOCAL_PATH)/omni_a8sqlteks.mk
